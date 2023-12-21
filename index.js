753a846bba4a0dd2a380b320e0c73868b6853c08
Victor <vicktorcori@hotmail.com>
2023-12-21 18:14:13 -0300

    Ejercicio 3.3

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ app.get('/info', (req, res) => {
   `)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const { id } = req.params;
+  const person = persons.find(person => person.id === Number(id))
+  
+  if(person) res.status(200).json(person)
+  else res.status(404).end()
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
