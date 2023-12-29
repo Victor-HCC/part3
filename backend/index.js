390aa39bc672319ca82ac891ab68e8ce312aa2e8
Victor <vicktorcori@hotmail.com>
2023-12-29 16:30:44 -0300

    Ejercicio 3.20-3.21

--- backend/index.js
+++ backend/index.js
@@ -110,7 +110,7 @@ app.put('/api/persons/:id', (req, res, next) => {
     name,
     number
   }
-  Person.findByIdAndUpdate(id, person, { new: true })
+  Person.findByIdAndUpdate(id, person, { new: true, runValidators: true })
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
